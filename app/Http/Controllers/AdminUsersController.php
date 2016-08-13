d31d6452114885ccd2da8f76c1eb913e1717a5a8
Tung <tung@test.com>
2016-08-14 01:05:55 +0700

    Done Edit User

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -9,6 +9,7 @@ use Illuminate\Http\Request;
 
 use App\Http\Requests;
 use App\Http\Requests\UsersRequest;
+use App\Http\Requests\UsersEditRequest;
 
 class AdminUsersController extends Controller
 {
@@ -84,6 +85,10 @@ class AdminUsersController extends Controller
     public function edit($id)
     {
         //
+		$user = User::findOrFail($id);
+		$roles = Role::lists('name', 'id')->all();
+		
+		return view('admin.users.edit', compact('user', 'roles'));
     }
 
     /**
@@ -93,9 +98,22 @@ class AdminUsersController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function update(UsersEditRequest $request, $id)
     {
         //
+		$user = User::findOrFail($id);
+		$input = $request->all();
+		
+		if($file = $request->file('photo_id')){
+			$name = time() . $file->getClientOriginalName();
+			$file->move('images', $name);
+			$photo = Photo::create(['file'=>$name]);
+			$input['photo_id'] = $photo->id;
+		}
+		
+		$user->update($input);
+		
+		 return redirect('admin/users');
     }
 
     /**
--- resources/views/admin/users/index.blade.php
+++ resources/views/admin/users/index.blade.php
@@ -6,6 +6,7 @@
     <thead>
       <tr>
         <th>Id</th>
+        <th>Photo</th>
         <th>Name</th>
         <th>Email</th>
         <th>Role</th>
@@ -20,7 +21,8 @@
 		@foreach($users as $user)
 		  <tr>
 			<td>{{$user->id}}</td>
-			<td>{{$user->name}}</td>
+			<td><img height="50px" src="{{URL::asset($user->photo ? $user->photo->file : '/images/user.png')}}" alt="User photo"></td>
+			<td><a href="{{route('admin.users.edit', $user->id)}}">{{$user->name}}</td>
 			<td>{{$user->email}}</td>
 			<td>{{$user->role->name}}</td>
 			<td>{{$user->is_active == 1 ? 'Active' : 'No Active' }}</td>
